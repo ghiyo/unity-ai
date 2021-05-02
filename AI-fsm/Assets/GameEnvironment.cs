8278587d35379bde7f9ba25bebdf59504408ead6
Malush <o.ghiyasi@gmail.com>
2021-05-02 02:01:31 -0400

    State machine for AI

---
+++ AI-fsm/Assets/GameEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnvironment : MonoBehaviour
+{
+  private static GameEnvironment instance;
+  private List<GameObject> _checkPoints = new List<GameObject>();
+  public List<GameObject> checkPoints = new List<GameObject>();
+
+  public static GameEnvironment Singleton
+  {
+    get
+    {
+      if (instance == null)
+      {
+        instance = new GameEnvironment();
+        instance.checkPoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+      }
+      return instance;
+    }
+  }
+}
