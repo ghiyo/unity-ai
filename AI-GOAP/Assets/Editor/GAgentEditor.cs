840cf2e2b4b94bacb628e940bb4b21beea3ae08e
Malush <o.ghiyasi@gmail.com>
2021-05-05 00:14:43 -0400

    Goal Oriented Action Plan

---
+++ AI-GOAP/Assets/Editor/GAgentEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(GAgentVisual))]
+[CanEditMultipleObjects]
+public class GAgentVisualEditor : Editor
+{
+
+
+  void OnEnable()
+  {
+
+  }
+
+  public override void OnInspectorGUI()
+  {
+    DrawDefaultInspector();
+    serializedObject.Update();
+    GAgentVisual agent = (GAgentVisual)target;
+    GUILayout.Label("Name: " + agent.name);
+    GUILayout.Label("Current Action: " + agent.gameObject.GetComponent<GAgent>().currentAction);
+    GUILayout.Label("Actions: ");
+    foreach (GAction a in agent.gameObject.GetComponent<GAgent>().actions)
+    {
+      string pre = "";
+      string eff = "";
+
+      foreach (KeyValuePair<string, int> p in a.preConditions)
+        pre += p.Key + ", ";
+      foreach (KeyValuePair<string, int> e in a.effects)
+        eff += e.Key + ", ";
+
+      GUILayout.Label("====  " + a.actionName + "(" + pre + ")(" + eff + ")");
+    }
+    GUILayout.Label("Goals: ");
+    foreach (KeyValuePair<SubGoal, int> g in agent.gameObject.GetComponent<GAgent>().goals)
+    {
+      GUILayout.Label("---: ");
+      foreach (KeyValuePair<string, int> sg in g.Key.sgoals)
+        GUILayout.Label("=====  " + sg.Key);
+    }
+    GUILayout.Label("Beliefs: ");
+    Dictionary<string, int> beliefs = agent.gameObject.GetComponent<GAgent>().beliefs.GetStates();
+    GUILayout.Label("---: ");
+    foreach (KeyValuePair<string, int> b in beliefs)
+      GUILayout.Label("=====  " + b.Key);
+    GUILayout.Label("Inventory: ");
+    List<GameObject> inventory = agent.gameObject.GetComponent<GAgent>().inventory.GetItems();
+    GUILayout.Label("---: ");
+    foreach (GameObject i in inventory)
+      GUILayout.Label("=====  " + i);
+    serializedObject.ApplyModifiedProperties();
+  }
+}
---
+++ AI-GOAP/Assets/GOAP/GAction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class GAction : MonoBehaviour
+{
+  public string actionName = "Action";
+  public float cost = 1.0f;
+  public GameObject target;
+  public string targetTag;
+  public float duration = 0;
+  public WorldState[] worldPreConditions;
+  public WorldState[] worldAfterEffects;
+  public NavMeshAgent agent;
+
+  public Dictionary<string, int> preConditions;
+  public Dictionary<string, int> effects;
+  public WorldStates beliefs;
+  public GInventory inventory;
+  public bool running = false;
+  public GAction()
+  {
+    preConditions = new Dictionary<string, int>();
+    effects = new Dictionary<string, int>();
+  }
+
+  public void Awake()
+  {
+    agent = this.gameObject.GetComponent<NavMeshAgent>();
+    if (worldPreConditions != null)
+    {
+      foreach (WorldState w in worldPreConditions)
+      {
+        preConditions.Add(w.key, w.value);
+      }
+    }
+    if (worldAfterEffects != null)
+    {
+      foreach (WorldState w in worldAfterEffects)
+      {
+        effects.Add(w.key, w.value);
+      }
+    }
+    inventory = this.GetComponent<GAgent>().inventory;
+    beliefs = this.GetComponent<GAgent>().beliefs;
+  }
+
+  public bool IsAchievable()
+  {
+    return true;
+  }
+
+  public bool IsAchieveableGiven(Dictionary<string, int> conditions)
+  {
+    foreach (KeyValuePair<string, int> p in preConditions)
+    {
+      if (!conditions.ContainsKey(p.Key)) return false;
+    }
+    return true;
+  }
+
+  public abstract bool PrePerform();
+  public abstract bool PostPerform();
+}
